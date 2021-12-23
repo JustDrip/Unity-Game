0d4e4de236b6c9900b99579d841ca41acbe6dd91
JustDrip <sebastiancremers8@gmail.com>
2021-12-23 16:30:30 +0100

    Finish line done

--- Schlaflabyrinth/Assets/Scripts/FinishLine.cs
+++ Schlaflabyrinth/Assets/Scripts/FinishLine.cs
@@ -6,20 +6,21 @@ using UnityEngine.UI;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] private Image EndScreen;
-
+    [SerializeField] private Text EndText;
+    [SerializeField] private Button EndButton;
+    [SerializeField] private Image EndImage;
+    [SerializeField] private Text EndText2;
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             EndScreen.enabled = true;
+            EndText.enabled = true;
+            EndButton.enabled = true;
+            EndImage.enabled = true;
+            EndText2.enabled = true;
+
         }   
     }
 
-    //void OnTriggerExit(Collider other)
-    //{
-    //    if (other.CompareTag("Player"))
-    //    {
-    //        EndScreen.enabled = false;
-    //    }
-    //}
 }
